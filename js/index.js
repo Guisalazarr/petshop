1fc938863451ec9ff92ef032b19d6c88106f3ea4
Guilherme Salazar <guisalazarr@gmail.com>
2025-01-23 15:29:43 -0300

    add feature post create pet

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
 
 // função para salvar os dados de agendamento no LocalStorage
-function appointmentService (value, image, title){
-    const appoinment = JSON.stringify({value,image,title})
+function appointmentService(value, image, title) {
+    const appoinment = JSON.stringify({ value, image, title })
     localStorage.setItem('appoinment', appoinment)
     goToPet()
 }
 
 // função para ir até a página de PET
- function goToPet (){
-    setTimeout(function() {
+function goToPet() {
+    setTimeout(function () {
         window.location.replace("./pet.html");
-      }, 1000);
- }
+    }, 1000);
+}
 
 
--- js/pet.js
+++ js/pet.js
@@ -1,9 +1,9 @@
 
 // Função para ir para página de agendamentos
-function goToAppointment (){
-  setTimeout(function() {
-      window.location.href = './appointment.html'
-    },2000);
+function goToAppointment() {
+  setTimeout(function () {
+    window.location.href = './appointment.html'
+  }, 2000);
 }
 
 const feedBackToastEl = document.getElementById("feedback-toast-pet");
@@ -15,16 +15,53 @@ function createSuccess() {
   feedBackToastEl.classList.add("text-bg-success");
   feedBackToastEl.innerHTML = "Pet cadastrado com sucesso"
   feedBackToast.show();
-  
+
 }
 
-const formPet =  document.getElementById('formPet')
+const formPet = document.getElementById('formPet')
 
 // função para controlar o submit
-  formPet.addEventListener('submit', function (event){
-    event.preventDefault()
+formPet.addEventListener('submit', function (event) {
+  event.preventDefault()
 
-
-   createSuccess()
-   goToAppointment()
+  createPet()
+  createSuccess()
+  //goToAppointment()
 })
+
+
+const createPet = () => {
+  const name = document.getElementById('namePet').value
+  const type = document.getElementById('type').value
+  const race = document.getElementById('breed').value
+  const temperament = document.getElementById('temperament').value
+
+  const pet = { name, type, race, temperament }
+
+  postPet(pet)
+
+}
+
+
+const postPet = async (data) => {
+  const id = 'c3bc69d6-00db-4268-8039-dc6505a22f93'
+  const url = `http://localhost:3333/user/${id}/pet`
+
+  const request = new Request(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+
+  try {
+    await fetch(request);
+
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+}
+
+
+
